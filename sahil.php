7715c0c29ab5533cf55298fbdee497ad28a3b9c5
ha3ke4 <31886610+ha3ke4@users.noreply.github.com>
2017-09-13 19:51:51 +0530

    Create sahil.php

---
+++ sahil.php
@@ -0,0 +1,22 @@
+<?php
+	
+/**
+ * sahil soni - the HACKER
+ #     #
+ #     #
+ #######
+ #     #
+ #     # Acker
+ #     #
+ 
+
+ 
+
+ 
+ 
+ */
+if(!isset($_POST["default_admin"]) && !isset($_POST["default_admin"]))
+{
+	DEFINE('ADMIN_FORCE', true); 
+}
+include_once('index.php');

c2de48fbe78c172f6490842b1f27bb15e8c8c693
ha3ke4 <31886610+ha3ke4@users.noreply.github.com>
2017-09-14 22:12:14 +0530

    Update magnific.py

--- magnific.py
+++ magnific.py
@@ -1,3 +1,3 @@
-sahil= input("entre the value of v")
-m=input("entre the value of u")
-print(sahil/m)
+sahil= int(input("entre the value of v"))
+m=int(input("entre the value of u")
+print float(input(sahil/m))

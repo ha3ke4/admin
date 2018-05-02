34ba5156b30f4300b1084fa8ab3bf8256375e042
sahil soni <31886610+ha3ke4@users.noreply.github.com>
2018-05-02 13:17:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-  return "Hello, World!"
+  return "<font color="green"><h1>Hello, World!</h1></font>"
   
 if __name__ == "__main__":
   app.run()
